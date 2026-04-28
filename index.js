d73763ec21da6a494d4da2a9824e1df4afec7f02
sanjib001 <sanjibpandey94@gmail.com>
2026-04-28 11:44:33 +0545

    Adds server setup

--- index.js
+++ index.js
@@ -2,39 +2,45 @@ import express from 'express'
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import 'dotenv/config'
+import serviceRoute from "./routes/sercives.js";
+import aboutRoute from "./routes/about-us.js";
+import portfolioRoute from "./routes/portfolios.js";
+import authRoute from "./routes/auth.js";
+import cors from 'cors';
 
-import serviceRoute from "./routes/sercives.js"
-import aboutRoute from "./routes/about-us.js"
-import herosectionRoute from "./routes/hero-section.js"
-import partnerRoute from "./routes/partner.js"
-import portfolioRoute from "./routes/portfolios.js"
-import authRoute from "./routes/auth.js"
-import cors from "cors";
+const app = express()
 
-const app = express();
-app.use(cors());
+app.use(cors())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded())
-app.use(express.static('public')); // use to serve static content(image,..) from BE to FE
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'));
 
-const { DATABASE_HOST, DATABASE_PORT, DATABASE_NAME, APPLICATION_PORT } = process.env;
+// app.use((req, res, next) => {
+//     logger.info(`${req.method} ${req.url}`);
+//     next();
+// });
 
-mongoose.connect(`mongodb://${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`)
-  .then(() => {
-    app.listen(APPLICATION_PORT, () => {
-      console.log(`Server is running on http://localhost:${APPLICATION_PORT}`)
+const { DATABASE_HOST, DATABASE_PORT, DATABASE_NAME, APPLICATION_PORT, CONNECTION_STRING } = process.env;
+
+try {
+    await mongoose.connect(CONNECTION_STRING, {
+        dbName: 'antbyte'
     })
-    console.log('sucessfully connected with Database !')
-  })
-  .catch((e) => console.log("failed to connect!!"));
+    app.listen(APPLICATION_PORT, () => {
+        console.log(`Server is running on http://localhost:${APPLICATION_PORT}`)
+    })
+    console.log('Successfully connected with database')
+} catch (e) {
+    console.log("failed to connect")
+}
 
-  app.get ("/", (req, res) =>{
-    res.send("Health check okay !!")
-  })
 
-  app.use("/services", serviceRoute);
-  app.use("/about", aboutRoute);
-  app.use("/herosection", herosectionRoute);
-  app.use("/partners", partnerRoute);
-  app.use("/portfolios", portfolioRoute);
-  app.use("/auth", authRoute);
+
+app.get("/", (req, res) => {
+    res.send("Health check okay!!")
+})
+
+app.use("/services", serviceRoute)
+app.use("/about", aboutRoute)
+app.use("/portfolios", portfolioRoute)
+app.use("/auth", authRoute)
